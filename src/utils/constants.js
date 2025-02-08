dac3d65158a99a573761bfbed1a6cd1f9fae59ed
ShaliniRanwa4 <ranwashalini4@gmail.com>
2025-02-08 19:55:31 +0530

    connect to backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 
 
 export const BASE_URL=
-location.hostname==="localhost"?"http://localhost:7777":"/api"
+location.hostname==="localhost"?"http://localhost:7777":"https://devtinder-production.up.railway.app"
